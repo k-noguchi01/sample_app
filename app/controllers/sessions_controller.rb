e89a7bf6aa2304d1f09f0776d188157eea1da65d
k-noguchi <k-noguchi@wwwave.jp>
2024-04-09 11:52:46 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       reset_session
+      # 一時ログインの場合にはforgetメソッドが呼ばれる
+      params[:session][:remember_me]=='1' ? remember(user):forget(user)
       log_in user
       redirect_to user
     else
@@ -15,9 +17,8 @@ class SessionsController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
-
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url,status: :see_other # see_other-->303 See Other
   end
 end
