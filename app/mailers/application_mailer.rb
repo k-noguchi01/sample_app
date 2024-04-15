ec83e542d1d9991b96fd589a007a8a857efcd89e
k-noguchi <k-noguchi@wwwave.jp>
2024-04-15 16:29:20 +0900

    fix mail domain bug

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  if Rails.env == 'develop'
-    default from: 'user@realdomain.com'
-  else
-    default from: 'k-noguchi@wwwave.jp'
-  end
+  default from: 'user@realdomain.com'
   layout 'mailer'
 end
